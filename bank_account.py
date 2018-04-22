76f69d11787d8c5fe1c733d99d7048aa1d35ece7
Marta Goljan <martagoljan@gmail.com>
2018-04-22 21:07:41 +0100

    Adding transactions to the account
    Adding transactions to the account transaction list.

--- bank_account.py
+++ bank_account.py
@@ -10,9 +10,13 @@ class BankAccount():
 		self.account_number = number
 		self.account_sortcode = sortcode
 		self.account_name = name
+		self.account_transactions_list = []
 
 	def __str__(self):
 		return "name %s, bank account %s, sortcode %s" % (self.account_name, self.account_number, self.account_sortcode)
+
+	def add_transaction(self, transaction):
+		self.account_transactions_list.append(transaction) #append(transaction) is adding transactions to the account_transaction_list (so you know what I bought)
 
 
 
@@ -27,3 +31,23 @@ print girl.account_currency
 print girl.account_name
 print boy.account_number
 print boy.account_name
+
+print girl.account_transactions_list
+
+
+
+sample_transaction = {"amount" : 2.50, "currency" : "GBP", "description" : "coffee", "date" : "April 2018"}
+girl.add_transaction(sample_transaction)
+print girl.account_transactions_list
+
+boy.add_transaction(sample_transaction)
+print boy.account_transactions_list
+
+
+
+
+
+
+
+
+
